30acbdb93330a20ed9751e2c677488cf47ad4e85
muxammadumar <musajonov.me@gmail.com>
2026-03-02 14:13:59 +0500

    feat(database): create users table with additional fields and unique index on telegramId

--- apps/api/prisma/migrations/20260227182041_init/migration.sql
+++ apps/api/prisma/migrations/20260227182041_init/migration.sql
@@ -1,2 +1,18 @@
--- CreateExtension
-CREATE EXTENSION IF NOT EXISTS "postgis";
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "telegramId" BIGINT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "username" TEXT,
+    "languageCode" TEXT,
+    "photoUrl" TEXT,
+    "isPremium" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_telegramId_key" ON "users"("telegramId");
